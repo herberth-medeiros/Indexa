168b2ede8e6a5234ae9ff6a49867fb0e6fbe543b
seven-beto <herberthfreitas26@gmail.com>
2025-05-22 23:13:03 -0300

    Criacao e estilizacao de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './components/separador/separador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalhoComponent, 
+    SeparadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
