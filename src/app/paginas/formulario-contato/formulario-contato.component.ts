994635761e410e49011220625150b9d1098c2744
seven-beto <herberthfreitas26@gmail.com>
2025-05-31 17:36:50 -0300

    FormGroup criado

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { SeparadorComponent } from '../../components/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -9,11 +10,24 @@ import { SeparadorComponent } from '../../components/separador/separador.compone
   imports: [
     CommonModule,
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
 
+  contatoForm!: FormGroup;
+
+  constructor(){
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Herberth'),
+      telefone: new FormControl('93 99999-9999'),
+      email: new FormControl('herberth@gmail'),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    })
+  }
 }
